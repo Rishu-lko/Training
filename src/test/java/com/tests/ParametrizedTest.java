3e352b66080dff483ac193cee0b8ba335a9c8dab
Ratnesh Singh <ratnesh.singh@globallogic.com>
2018-06-14 17:36:56 +0530

    Changes in Automation branch

--- src/test/java/com/tests/ParametrizedTest.java
+++ src/test/java/com/tests/ParametrizedTest.java
@@ -17,22 +17,22 @@ public class ParametrizedTest extends BaseClass{
 	}
 	
 	@Test(dataProvider="getData",groups= {"data"})
-	public void parametrizedTestMultiValue(String argument1, @Optional("Ratnesh") String argument2) {		
+	public void parametrizedTestMultiValue(int argument1,int argument2) {		
 		APP_LOG.info(argument1 +" , "+argument2);
 		
 	}
 	
 	@DataProvider(name="getData")
-	public String[][] datafetch(ITestContext context) {
+	public Integer[][] datafetch(ITestContext context) {
 	//Map<String,String> localParameters = context.getCurrentXmlTest().getLocalParameters();
 	//Map<String,String> allParameters = context.getCurrentXmlTest().getAllParameters();
 	//Map<String,String> allParameters = context.getCurrentXmlTest().get
 	String parameter = context.getCurrentXmlTest().getAllParameters().get("parameter");
 	String[] paramerarray=	parameter.split(",");
-	String[][] returnValues = new String[paramerarray.length][2];
+	Integer[][] returnValues = new Integer[paramerarray.length][2];
 	for(int i =0;i<paramerarray.length;i++) {
-		returnValues[i][0]=paramerarray[i];
-		//returnValues[i][1]=paramerarray[i];
+		returnValues[i][0]=Integer.valueOf(i);
+		returnValues[i][1]=Integer.valueOf(paramerarray[i]);
 		
 	}
 	return returnValues;		
