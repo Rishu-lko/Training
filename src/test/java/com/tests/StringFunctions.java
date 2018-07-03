8aad7049a7b0c55bb3d4290ed8854588eb0842f5
Ratnesh Singh <ratnesh.singh@globallogic.com>
2018-07-03 14:30:50 +0530

    Changed Commit Message using Rebase

---
+++ src/test/java/com/tests/StringFunctions.java
@@ -0,0 +1,13 @@
+package com.tests;
+
+import com.framework.BaseClass;
+import org.testng.annotations.Test;
+
+public class StringFunctions extends BaseClass {
+	
+	@Test
+	public void checkStringReplace() {
+		
+	}
+
+}
