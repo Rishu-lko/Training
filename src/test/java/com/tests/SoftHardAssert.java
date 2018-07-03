fadebcb18136cf26382e1874ab2c5f0a467a5354
Ratnesh Singh <ratnesh.singh@globallogic.com>
2018-07-03 14:30:50 +0530

    Changed Again Commit Message

---
+++ src/test/java/com/tests/SoftHardAssert.java
@@ -0,0 +1,43 @@
+package com.tests;
+import com.framework.BaseClass;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class SoftHardAssert extends BaseClass{
+	
+	@Test
+	@SuppressWarnings("unused")
+	public void verifyHardReset(){
+		try {
+		int rat = 1;
+		APP_LOG.info("Before Hard Reset Exception");
+		Assert.assertTrue(false);
+		APP_LOG.info("After Hard Reset Exception");
+		}
+		catch(AssertionError e) {
+			
+			APP_LOG.info("AssertionError Exception Catched");
+		}
+		
+	}
+	
+	@Test
+	public void verifySoftReset(){
+		try {
+		SoftAssert softAssert = new SoftAssert();
+		softAssert.assertTrue(false);
+		APP_LOG.info("Before Soft Reset Exception");
+		softAssert.assertTrue(false);
+		APP_LOG.info("After Soft Reset Exception");
+		softAssert.assertAll();
+		}
+		
+catch(AssertionError e) {
+			
+			APP_LOG.info("AssertionError Exception Catched");
+		}
+		
+	}
+
+}
